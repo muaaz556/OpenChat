1d1e6bacb8f119629446c7e08c2190a10b008d91
MUAAZ MAHMUD <m7mahmud@uwaterloo.ca>
2023-05-11 23:37:38 -0400

    Temporary UI changes

--- MessagingApp/Hubs/ChatHub.cs
+++ MessagingApp/Hubs/ChatHub.cs
@@ -4,6 +4,36 @@ namespace MessagingApp.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly static Dictionary<string, string> _connections =
+            new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            string user = Guid.NewGuid().ToString();
+            lock (_connections)
+            {
+                var users = _connections.Values.ToArray();
+                _connections.Add(Context.ConnectionId, user);
+                Clients.AllExcept(new List<string>() { Context.ConnectionId }).SendAsync("NewUser", user);
+                //var users = _connections.Values.ToArray();
+                //users = users.Where(x => x != _connections[Context.ConnectionId]).ToArray();
+                Clients.Client(Context.ConnectionId).SendAsync("UserList", users);
+            }
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            lock (_connections)
+            {
+                Clients.All.SendAsync("RemoveUser", _connections[Context.ConnectionId]);
+                _connections.Remove(Context.ConnectionId);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
--- MessagingApp/wwwroot/js/chat.js
+++ MessagingApp/wwwroot/js/chat.js
@@ -2,8 +2,11 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
+
+let usersList = [];
+
 //Disable the send button until connection is established.
-document.getElementById("sendButton").disabled = true;
+//document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
@@ -12,6 +15,54 @@ connection.on("ReceiveMessage", function (user, message) {
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
     li.textContent = `${user} says ${message}`;
+});
+
+connection.on("UserList", function (list) {
+    console.log("list " + list);
+    usersList = usersList.concat(list);
+    list.forEach(addElement);
+    //var li = document.createElement("li");
+    //document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    //li.textContent = `${user} says ${message}`;
+});
+
+function addElement(user) {
+    document.getElementById('scrollView').innerHTML += (
+        '<div id="' + user + '" class="row m-2" style="border-style: solid; border-radius: 20px;">' +
+        '<span class="m-2 align-middle">' + user + '</span>' +
+        '</div > ');
+}
+
+function removeElement(user) {
+    const element = document.getElementById(user);
+    element.remove();
+}
+
+connection.on("NewUser", function (user) {
+    console.log("new user " + user);
+    usersList.push(user);
+    addElement(user);
+    //var li = document.createElement("li");
+    //document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    //li.textContent = `${user} says ${message}`;
+});
+
+connection.on("RemoveUser", function (user) {
+    console.log(user);
+    usersList = usersList.filter(item => item !== user);
+    removeElement(user);
+    //var li = document.createElement("li");
+    //document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    //li.textContent = `${user} says ${message}`;
 });
 
 connection.start().then(function () {
